1ad78693d3d977483bf36aa41adf21efd2d601fc
yudwns <realyoung95@gmail.com>
2024-09-14 12:21:02 +0900

    Create style.py

---
+++ style.py
@@ -0,0 +1,21 @@
+def load_css():  
+    return """  
+    <style>  
+    .stApp {  
+        background-color: #f0f2f6;  
+    }  
+    .stButton>button {  
+        color: #4f8bf9;  
+        border-radius: 50px;  
+        height: 3em;  
+        width: 200px;  
+    }  
+    </style>  
+    """  
+
+def load_header():  
+    return """  
+    <div style="background-color:#f1f1f1;padding:20px;border-radius:10px;">  
+        <h1 style="color:#1f77b4;text-align:center;">PDF Summarizer</h1>  
+    </div>  
+    """
